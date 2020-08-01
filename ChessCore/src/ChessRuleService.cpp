1c1997703cdd84ed0f50f4a88b4f8b4650fc2b90
Alex Fedorov <afedorov@amazon.com>
2020-08-01 14:36:22 -0700

    move is improper if no piece on the square

--- ChessCore/src/ChessRuleService.cpp
+++ ChessCore/src/ChessRuleService.cpp
@@ -10,6 +10,10 @@ bool ChessRuleService::isValidMove(const Move &move, const Board &board) {
 
     const Position &position = board.get(move.startColumn, move.startRow);
 
+    if (!position.hasPiece) {
+        return false;
+    }
+
     if (position.piece.pieceType == PieceType::POND) {
         if (position.piece.pieceColor == PieceColor::WHITE) {
             if (move.startColumn == move.endColumn && move.endRow - move.startRow == 1) {
--- ChessCore/test/include/ChessRuleServiceTest.h
+++ ChessCore/test/include/ChessRuleServiceTest.h
@@ -9,18 +9,28 @@
 #include <Move.h>
 
 BOOST_AUTO_TEST_CASE(proper_white_pond_move_on_one_square) {
-    Board board;
+    Board board = createStandardBoard();
     ChessRuleService chessRuleService;
 
     BOOST_CHECK(chessRuleService.isValidMove(Move{E, _2, E, _3}, board));
 }
 
 BOOST_AUTO_TEST_CASE(proper_black_pond_move_on_one_square) {
-    Board board;
+    Board board = createStandardBoard();
     ChessRuleService chessRuleService;
 
     BOOST_CHECK(chessRuleService.isValidMove(Move{E, _7, E, _6}, board));
 }
+
+
+BOOST_AUTO_TEST_CASE(improper_move_when_there_is_no_piece) {
+    Board board = createStandardBoard();
+    ChessRuleService chessRuleService;
+
+    BOOST_CHECK(!chessRuleService.isValidMove(Move{E, _4, E, _5}, board));
+}
+
+
 
 
 #endif //ANYWHERECHESS_CHESSRULESERVICETEST_H
